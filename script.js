a5045015ffa08a2503ce7d0398a34f756c3cc81b
Tristenh <tristenhobba1@gmail.com>
2023-06-15 15:50:19 +1000

    added HTML buttons and form, getElementById for all ids

---
+++ script.js
@@ -0,0 +1,5 @@
+const start = document.getElementById("start");
+const clickMe = document.getElementById("click-me");
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const submit = document.getElementById("submit");
