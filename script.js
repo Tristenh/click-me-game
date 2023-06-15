c1e4ac0d88b7e55e0cdfb15359120a33fd127371
Tristenh <tristenhobba1@gmail.com>
2023-06-15 15:55:30 +1000

    added p element id score, added count and time variables, clickStart () when clicked if time is 0 count ++ score = count, start click event , starts clickStart, setInterval time -- if time is 0 clearInterval

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ const clickMe = document.getElementById("click-me");
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const submit = document.getElementById("submit");
+const score = document.getElementById("score");
+
+let count = 0;
+let time = 5;
+
+function clickStart() {
+  clickMe.addEventListener("click", function () {
+    if (time > 0) {
+      count++;
+      score.innerHTML = count;
+      return count;
+    }
+  });
+}
+
+start.addEventListener("click", function () {
+  clickStart();
+  start.style.display = "none";
+  let startTime = setInterval(function () {
+    time--;
+    console.log(time);
+    if (time === 0) {
+      clearInterval(startTime);
+    }
+  }, 1000);
+});
